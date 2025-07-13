d6aef6d7657af64dafd5930d859b623c2ad56d15
Pulkit Bohra <162800650+PulkitBohra@users.noreply.github.com>
2025-07-13 14:57:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import CreateEventDrawer from "@/components/create-event";
+import { Suspense } from 'react'; 
 
 export const metadata = {
   title: "MeetMate",
@@ -17,7 +18,9 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} flex min-h-screen flex-col`}>
         <Header />
         <main className="flex-grow bg-gradient-to-b from-blue-50 to-white">
-          {children}
+          <Suspense fallback={<div>Loading...</div>}>
+              {children}
+            </Suspense>
         </main>
         <footer className="bg-blue-100 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
