745f2e9b8451ea8a97fbcd83f76032d40ce7b192
Pulkit Bohra <162800650+PulkitBohra@users.noreply.github.com>
2025-07-13 19:46:44 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,21 +1,7 @@
 import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/events(.*)",
-  "/meetings(.*)",
-  "/availability(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    const { userId } = auth();
-    if (!userId) {
-      return NextResponse.redirect(new URL("/sign-in", req.url));
-    }
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
