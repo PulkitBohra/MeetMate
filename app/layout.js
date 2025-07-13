90d25df23a3bc9469cb1fdce1eccbc2b86a8e482
Pulkit Bohra <162800650+PulkitBohra@users.noreply.github.com>
2025-07-13 15:09:36 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,37 @@
+// app/layout.jsx
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import CreateEventDrawer from "@/components/create-event";
-import { Suspense } from 'react'; 
+import { Suspense } from 'react';
 
 export const metadata = {
   title: "MeetMate",
   description: "Seamless Meeting Booking and Conflict Resolution Tool",
 };
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className} flex min-h-screen flex-col`}>
-        <Header />
-        <main className="flex-grow bg-gradient-to-b from-blue-50 to-white">
+      <html lang="en">
+        <body className={`${inter.className} flex min-h-screen flex-col`}>
           <Suspense fallback={<div>Loading...</div>}>
+            <Header />
+            <main className="flex-grow bg-gradient-to-b from-blue-50 to-white">
               {children}
-            </Suspense>
-        </main>
-        <footer className="bg-blue-100 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made with ❤️ by Pulkit Bohra</p>
-          </div>
-        </footer>
-      <CreateEventDrawer/>
-
-      </body>
-    </html>
+            </main>
+            <footer className="bg-blue-100 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-600">
+                <p>Made with ❤️ by Pulkit Bohra</p>
+              </div>
+            </footer>
+            <CreateEventDrawer/>
+          </Suspense>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
